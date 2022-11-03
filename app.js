d5abd588b55364699b906c79c6934806aa20e5d0
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-03 19:07:41 -0400

    feat: Add mongoose

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ import formRouter from './routes/form.js'
 const app = express()
 const port = process.env.PORT || 8080
 
-app.use(urlencoded({ extended: false }));
+app.use(urlencoded({ extended: false }))
 app.use(json())
 
-app.use("/api", formRouter)
+app.use('/api', formRouter)
 
-app.listen('8080', () => {
-  console.log('http://localhost:8080')
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log('http://localhost:8080')
+    })
+  })
+  .catch(() => {
+    console.error(err)
+  })
