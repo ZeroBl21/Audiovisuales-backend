f2d80efd2559ef26dadb43ffef91d887e5df42d2
Darwin Perdomo <ZeroBl@hotmail.com>
2022-11-27 23:54:58 -0400

    feat(controllers/form): Add base send email to user

---
+++ utils/email.js
@@ -0,0 +1,38 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import Sib from 'sib-api-v3-sdk'
+
+export function sendEmail(targetUser, targetMail, reservationId) {
+  const date = new Date()
+  const today = date.toISOString().split('T')[0]
+  console.log(targetUser, targetMail, reservationId)
+
+  const client = Sib.ApiClient.instance
+  const apiKey = client.authentications['api-key']
+  apiKey.apiKey = process.env.MAIL_APIKEY
+
+  const transEmailApi = new Sib.TransactionalEmailsApi()
+
+  const sender = {
+    email: process.env.MAIL_SENDER,
+  }
+
+  const receivers = [
+    {
+      email: targetMail,
+    },
+  ]
+
+  return transEmailApi.sendTransacEmail({
+    sender,
+    to: receivers,
+    templateId: 2,
+    params: {
+      name: targetUser,
+      id: reservationId,
+      date: today,
+      use: date.toISOString(),
+    },
+  })
+}
